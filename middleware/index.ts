0967548a3d4c161c3b1aeb11ec9477feb2e79e73
Olga <olyakrylo24@gmail.com>
2022-01-14 14:10:47 +0300

    move getting user to client side

--- middleware/index.ts
+++ middleware/index.ts
@@ -1,35 +1,4 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { User } from "../utils/types";
-import Cookies from "cookies";
-import { getCookie } from "../utils/cookies";
-import { connect } from "../utils/connection";
-import { dataExample } from "../data.example";
-
 type Method = "get" | "post" | "put" | "delete";
-
-export async function getUser(
-  req: NextApiRequest,
-  res: NextApiResponse
-): Promise<User | null> {
-  const cookies = new Cookies(req, res);
-  const { SECRET, NO_CONNECTION } = process.env;
-
-  if (NO_CONNECTION) {
-    return dataExample.user;
-  }
-
-  const id = getCookie(cookies, "id_token", SECRET as string);
-  if (id) {
-    const { User } = await connect();
-    const user = await User.findById(id);
-    return {
-      ...user._doc,
-      _id: id,
-    };
-  }
-
-  return null;
-}
 
 export async function request<T = any>(
   method: Method,
