1bdf101d9aa8ffa9ffabd9ebb3c6c6642fa5b2b9
Olga <olyakrylo24@gmail.com>
2022-04-24 17:24:02 +0400

    add fonts preloading

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,27 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png" />
           <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="preload"
+            href="/fonts/Roboto-Light.woff"
+            as="font"
+            type="font/woff"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/Roboto-Regular.woff"
+            as="font"
+            type="font/woff"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/NotoSans-Bold.woff"
+            as="font"
+            type="font/woff"
+            crossOrigin=""
+          />
           <meta name="theme-color" content="#1a8f88" />
         </Head>
         <body>
